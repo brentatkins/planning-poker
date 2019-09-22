97db93b5e3b2f929884bfb1fdfce622117c37ef4
Brent <brentatkins@gmail.com>
2019-09-22 09:10:23 +0200

    Updates

---
+++ precache-manifest.052bf2ce069a222b54501da5f91991ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1730a1920689122f3e3737bcc4b3d65",
+    "url": "/planning-poker/index.html"
+  },
+  {
+    "revision": "0f7012ea3c10029afee0",
+    "url": "/planning-poker/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f1ab0ddef10314ed60e2",
+    "url": "/planning-poker/static/js/2.4205bd0e.chunk.js"
+  },
+  {
+    "revision": "0f7012ea3c10029afee0",
+    "url": "/planning-poker/static/js/main.422ffd98.chunk.js"
+  },
+  {
+    "revision": "67c48b08c715cc40cf8b",
+    "url": "/planning-poker/static/js/runtime-main.9851197a.js"
+  }
+]);
