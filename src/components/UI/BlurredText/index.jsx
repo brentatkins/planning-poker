873b963c704962931a42797294e1ced45d799188
Brent <brentatkins@gmail.com>
2019-11-09 15:30:31 +0100

    use css transition on blurred text

--- src/components/UI/BlurredText/index.jsx
+++ src/components/UI/BlurredText/index.jsx
@@ -2,6 +2,11 @@ import { Text } from "grommet";
 import styled from "styled-components";
 
 export const BlurredText = styled(Text)`
+  transition: ${props =>
+    props.blurSize !== 0
+      ? "none"
+      : "filter 1s"}; // only apply transition when changing from blurred to not blurred
+  transition-timing-function: ease-in;
   filter: blur(
     ${props =>
       props.blurSize !== false && props.blurSize >= 0 ? props.blurSize : 15}px
